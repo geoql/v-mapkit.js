44c40d86696029cff4d4c60b47a4cbc09430bf36
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-06-25 23:50:13 +0530

    feat: minor housekeeping of utils

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export const VMapkit = {};
+import { App as Application, Plugin } from 'vue';
+import VMap from './components/VMap.vue';
+import { setVueInstance } from './utils/config/index';
+
+let installed: boolean = false;
+
+const install: Exclude<Plugin['install'], undefined> = (
+  instance: Application,
+) => {
+  if (!installed) {
+    setVueInstance(instance);
+    instance.component('v-map', VMap);
+    installed = true;
+  }
+};
+
+export default install;
+
+export { VMap };
