4c8ea1921de72c7d44b50c10c98c7e1d8f7dc26a
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-07-30 19:47:24 +0530

    feat(ts): strictly type props

--- src/components/VMap.vue
+++ src/components/VMap.vue
@@ -12,6 +12,7 @@
     reactive,
   } from 'vue';
   import { loadMapKit } from '../utils/helpers';
+  import { VMapProps } from '~/types';
 
   export default defineComponent({
     name: 'VMap',
@@ -50,7 +51,7 @@
         },
       },
     },
-    setup(props) {
+    setup(props: VMapProps) {
       let init: Ref<boolean> = ref(false);
       let mapkit: typeof window.mapkit = reactive({} as typeof window.mapkit);
       const state = reactive({
